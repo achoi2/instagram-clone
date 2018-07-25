f67231a176faba8fbbd3a2786de5fdd44bc6e19d
Andrew <andrewchoi1@hotmail.com>
2018-07-25 11:39:34 -0400

    changed outline

--- main.js
+++ main.js
@@ -1,73 +1,66 @@
 var images = [
-  {
-    description: "dog1",
-    link:
-      "http://cdn3-www.dogtime.com/assets/uploads/2011/03/puppy-development-460x306.jpg"
-  },
-  {
-    description: "dog2",
-    link:
-      "https://static.scientificamerican.com/sciam/cache/file/D059BC4A-CCF3-4495-849ABBAFAED10456.jpg?w=590&h=393&526ED1E1-34FF-4472-B348B8B4769AB2A1"
-  },
-  {
-    description: "dog3",
-    link: "http://cdn2-www.dogtime.com/assets/uploads/2016/08/corgi-puppy-6.jpg"
-  },
-  {
-    description: "dog4",
-    link: "http://cdn.akc.org/content/hero/puppy-boundaries_header.jpg"
-  },
-  {
-    description: "dog5",
-    link: "http://pawsitivepuppies.com/wp-content/uploads/2011/08/DSC_7822.jpg"
-  },
-  {
-    description: "dog6",
-    link: "http://elcamion.co.uk/assets/2015/03/Chihuahua-Puppies.jpg"
-  }
+    {
+        description: 'dog1',
+        link:
+            'http://cdn3-www.dogtime.com/assets/uploads/2011/03/puppy-development-460x306.jpg'
+    },
+    {
+        description: 'dog2',
+        link:
+            'https://static.scientificamerican.com/sciam/cache/file/D059BC4A-CCF3-4495-849ABBAFAED10456.jpg?w=590&h=393&526ED1E1-34FF-4472-B348B8B4769AB2A1'
+    },
+    {
+        description: 'dog3',
+        link:
+            'http://cdn2-www.dogtime.com/assets/uploads/2016/08/corgi-puppy-6.jpg'
+    },
+    {
+        description: 'dog4',
+        link: 'http://cdn.akc.org/content/hero/puppy-boundaries_header.jpg'
+    },
+    {
+        description: 'dog5',
+        link:
+            'http://pawsitivepuppies.com/wp-content/uploads/2011/08/DSC_7822.jpg'
+    },
+    {
+        description: 'dog6',
+        link: 'http://elcamion.co.uk/assets/2015/03/Chihuahua-Puppies.jpg'
+    }
 ];
 
-var header = document.querySelector(".header-text");
-header.textContent = "PuppyGram";
+var header = document.querySelector('.header-text');
+header.textContent = 'PuppyGram';
 
-var mainContainer = document.querySelector(".main-container");
+var mainContainer = document.querySelector('.main-container');
 
-var modal = document.querySelector(".myModal");
-var button = document.querySelector(".mybutton");
+var modal = document.querySelector('.myModal');
+var closeButton = document.querySelector('.close');
 
-var span = document.querySelector(".close");
-
-button.addEventListener("click", function() {
-  modal.style.display = "block";
-});
-
-span.addEventListener("click", function() {
-  modal.style.display = "none";
-});
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+var toggleHandler = function() {
+    modal.classList.toggle('show-modal');
 };
 
-for (var image of images) {
-  var listItem = document.createElement("li");
-  listItem.classList.add("dog-list");
+// mainContainer.addEventListener("click", function() {
+//   modal.style.display = "block";
+// });
 
-  var puppyImage = document.createElement("img");
-  puppyImage.setAttribute("src", image.link);
+closeButton.addEventListener('click', toggleHandler);
 
-  var description = document.createElement("h3");
-  description.textContent = image.description;
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
 
-  listItem.appendChild(puppyImage);
-  listItem.appendChild(description);
-  mainContainer.appendChild(listItem);
+for (var i = 0; i < images.length; i++) {
+    var selectImage = document.querySelector('.myModal');
+    var thumbNailDiv = document.createElement('div');
+    var newImage = document.createElement('img');
+
+    newImage.src = images[i].link;
+    thumbNailDiv.setAttribute('data-index', i);
+
+    selectImage.appendChild(thumbNailDiv);
+    thumbNailDiv.appendChild(newImage);
 }
-
-var lightBoxHandler = function(event) {
-  console.log("element clicked");
-};
-
-mainContainer.addEventListener("click", lightBoxHandler);
